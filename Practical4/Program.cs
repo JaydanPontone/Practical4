3d5fc3230d2558073cf776940e46860aed9de9fc
Jaydan Pontone <40438152@live.napier.ac.uk>
2021-10-05 16:36:16 +0100

    Added hello napier

--- Practical4/Program.cs
+++ Practical4/Program.cs
@@ -7,6 +7,7 @@ namespace Practical4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Napier!");
         }
     }
 }
